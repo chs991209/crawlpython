cac210c08d2b42c05bb7b5e4aa665c32d2078a8a
Choihyunsu <chs991209@gmail.com>
2021-12-03 10:24:24 +0900

    naver image로 url 수정, save path 경로 수정(관리자 권한 범위 경로)

--- 1st.py
+++ 1st.py
@@ -2,13 +2,13 @@ import urllib.request as req
 
 
 # File_url
-img_url = 'https://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-mediumSquareAt3X-v2.jpg'
-html_url = 'https://www.google.com'
+img_url = 'https://postfiles.pstatic.net/MjAyMTExMThfMTIz/MDAxNjM3MjIyNTEzMjIx.1tuKSq0QSVAwpt-XYQC5324zo9Nm1SbUaHYRzXwxKKMg.ed8BjgKqvXb2StrHvEdHlo7TdojJGJX9__qC_1fQ5REg.JPEG.soravan/SE-f8502d21-8003-47b9-9f89-68853cbab66f.jpg?type=w773'
+html_url = 'https://www.naver.com'
 
 
 # Download path
-save_path1 = '~/test1.jpg'
-save_path2 = '~/index.html'
+save_path1 = '/Users/choehyeonsu/test2.jpg'
+save_path2 = '/Users/choehyeonsu/index2.html'
 
 # Exception
 try:
