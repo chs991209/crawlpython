f31f483fe44712870574082d6b2e07fc5aa1b499
ChoiHs <chs991209@gmail.com>
2024-01-22 16:43:53 +0900

    MODIFY: urllib.request library requests로 대체

--- Fake_User_Agent/07_Basic_scrapping_Prac.py
+++ Fake_User_Agent/07_Basic_scrapping_Prac.py
@@ -3,8 +3,7 @@
 # Daum Stock Info
 
 import json
-import urllib.request as req
-
+import requests
 from fake_useragent import UserAgent
 
 # Fake Headers Info
@@ -25,28 +24,22 @@ headers = {
 # Daum Stock Info requesting url
 url = "https://finance.daum.net/api/search/ranks?limit=10"
 
-# Request Code
-# def Request(url, data=None, headers={},..
-# headers={} 에서 dict 을 대입하여 User-agent, Referer 모두 브라우저의 header 값과 동일하게 한다.
-# url 은 ajax 로 받는 Stock Info 요청 url 을 입력
-res = req.urlopen(req.Request(url, headers=headers)).read().decode("UTF-8")
 
-# Requested Data Check (Json Data)
-print("res :", res)
+response = requests.get(url, headers=headers)
 
-# Requested Data String -> Json Trans and Data Print
+if response.status_code == 200:
+    res = response.text
 
-rank_json = json.loads(res)["data"]
+    # Requested Data Check (Json Data)
+    print("res:", res)
 
-# Mid Check
-print("Mid Check : ", rank_json, "\n")
+    # Requested Data String -> Json Trans and Data Print
+    rank_json = json.loads(res)["data"]
 
+    # Mid Check
+    print("Mid Check:", rank_json, "\n")
 
-class RankJson:
     for elm in rank_json:
-        # print('순위 : {}, 금액 : {}, 사명 : {}, 변동 양상 : {}'.format(elm['rank'], elm['tradePrice'], elm['name'], elm['change']))
-        # # print('')
-
         name = elm["name"]
         rank = elm["rank"]
         tradePrice = elm["tradePrice"]
@@ -60,3 +53,5 @@ class RankJson:
         print("")
 
     print(type(elm))
+else:
+    print("Failed to retrieve data. Status code:", response.status_code)
